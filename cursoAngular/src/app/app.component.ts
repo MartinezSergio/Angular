1c23f0f061cd2a385d66373dd4b31447f908b8ae
MartinezSergio <soysergiom@gmail.com>
2018-11-19 16:11:10 +0100

    Insercion de evento en objeto

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   styles: []
-
 })
-
-
 export class AppComponent {
-  title = 'cursoAngular';
+  title = "cursoAngular";
   cursos = [
-    {'id': 1, 'nombre': 'Beca Java', 'duracion': 120},
-    {'id': 2, 'nombre': 'Beca Angular', 'duracion': 4},
-    {'id': 3, 'nombre': 'Beca Java Spring', 'duracion': 8}
-];
+    { id: 1, nombre: "Beca Java", duracion: 120 },
+    { id: 2, nombre: "Beca Angular", duracion: 4 },
+    { id: 3, nombre: "Beca Java Spring", duracion: 8 }
+  ];
+
+  seleccionar(curso) {
+    console.log(curso);
+  }
 }
