a8e0f62909ee4fd5cce834a0e4c8845888f08f55
MartinezSergio <soysergiom@gmail.com>
2018-11-19 17:08:54 +0100

    Date con formateo

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -8,9 +8,27 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title = "cursoAngular";
   cursos = [
-    { id: 1, nombre: "Beca Java", duracion: 120 },
-    { id: 2, nombre: "Beca Angular", duracion: 4 },
-    { id: 3, nombre: "Beca Java Spring", duracion: 8 }
+    {
+      id: 1,
+      nombre: "Beca Java",
+      duracion: 120,
+      precio: 700,
+      fecha: new Date()
+    },
+    {
+      id: 2,
+      nombre: "Beca Angular",
+      duracion: 4,
+      precio: 30,
+      fecha: new Date()
+    },
+    {
+      id: 3,
+      nombre: "Beca Java Spring",
+      duracion: 8,
+      precio: 60,
+      fecha: new Date()
+    }
   ];
 
   seleccionar(curso) {
--- cursoAngular/src/app/app.componente2.ts
+++ cursoAngular/src/app/app.componente2.ts
@@ -1,22 +1,15 @@
-@Component ({
-  selector: 'selector-dos',
-  templateUrl: './app.componente.html',
-  styleUrls: ['./style.css']
-})
-
-
-getCSSClasses(modo: string) {
+function getCSSClasses(modo: string) {
   let cssClasses;
-  if(modo == 'noche') {
+  if (modo == "noche") {
     cssClasses = {
-      'one': true,
-      'two': true
-    }
+      one: true,
+      two: true
+    };
   } else {
     cssClasses = {
-      'two': true,
-      'four': false
-    }
+      two: true,
+      four: false
+    };
   }
   return cssClasses;
 }
