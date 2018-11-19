8000f006004655d34d5fcce6def4a323776d8b5d
MartinezSergio <soysergiom@gmail.com>
2018-11-19 15:44:28 +0100

    IF en Angular

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   title = 'cursoAngular';
   cursos = [
     {'id': 1, 'nombre': 'Beca Java', 'duracion': 120},
-    {'id': 2, 'nombre': 'Beca Angular', 'duracion': 16},
+    {'id': 2, 'nombre': 'Beca Angular', 'duracion': 4},
     {'id': 3, 'nombre': 'Beca Java Spring', 'duracion': 8}
 ];
 }
