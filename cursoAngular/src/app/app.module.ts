f875276fc0d68687845ccf3b4f78080a9daef01a
MartinezSergio <soysergiom@gmail.com>
2018-11-20 11:31:34 +0100

    Creando nuevos componentes

--- cursoAngular/src/app/app.module.ts
+++ cursoAngular/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { MultiplicadorPipe } from './multiplicador.pipe';
+import { MultiplicadorPipe } from "./multiplicador.pipe";
+import { LoggerComponentComponent } from "./logger-component/logger-component.component";
+import { TracerComponent } from "./tracer/tracer.component";
 
 @NgModule({
   declarations: [
-    AppComponent, MultiplicadorPipe
+    AppComponent,
+    MultiplicadorPipe,
+    LoggerComponentComponent,
+    TracerComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ cursoAngular/src/app/tracer/tracer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracer',
+  templateUrl: './tracer.component.html',
+  styles: []
+})
+export class TracerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
